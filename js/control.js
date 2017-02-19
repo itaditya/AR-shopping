a7cde3f93baa18b06a613b1f50d938f1caf7b8ad
itaditya <adityaa803@gmail.com>
2017-02-19 12:53:50 +0530

    3d view

--- js/control.js
+++ js/control.js
@@ -29,6 +29,8 @@ var autoplay = function () {
             context = canvas.getContext('2d'),
             video = document.createElement('video'),
             fist_pos_old,
+            angle = [0, 0],
+            smoother = new Smoother([0.9995, 0.9995], [0, 0], 0),
             detector;
         document.getElementsByTagName('body')[0].appendChild(canvas);
         var gestureHtml = '<div id="gestureDiv"><img class="img-responsive" src="/img/right.gif"></div>';
@@ -44,12 +46,7 @@ var autoplay = function () {
                     video.src = stream;
                 }
                 console.log($("#myModal").css('display'));
-                if ($("#myModal").css('display') == 'block') {
-                    compatibility.requestAnimationFrame(rotateplay);
-                } else {
-                    compatibility.requestAnimationFrame(play);
-                }
-
+                compatibility.requestAnimationFrame(play);
             }, function (error) {
                 alert("WebRTC not available");
             });
@@ -66,6 +63,11 @@ var autoplay = function () {
                     var width = ~~(80 * video.videoWidth / video.videoHeight);
                     var height = 80;
                     detector = new objectdetect.detector(width, height, 1.1, objectdetect.handfist);
+                }
+                angle = smoother.smooth(angle);
+                if ($(".3d")) {
+                    document.getElementById('transform_a').setAttribute('rotation', '0 1 0 ' + angle[0]);
+                    document.getElementById('transform_b').setAttribute('rotation', '1 0 0 ' + angle[1]);
                 }
                 /* Draw video overlay: */
                 canvas.width = ~~(100 * video.videoWidth / video.videoHeight);
@@ -88,7 +90,10 @@ var autoplay = function () {
                     if (fist_pos_old) {
                         var dx = (fist_pos[0] - fist_pos_old[0]) / video.videoWidth,
                             dy = (fist_pos[1] - fist_pos_old[1]) / video.videoHeight;
-                        var ele = $("#mainImage")[0];
+                        if (dx * dx + dy * dy < 0.2) {
+                            angle[0] += 0.4 * dx;
+                            angle[1] += 0.4 * dy;
+                        }
                         var swiperElem = $(".swiper");
                         $.each(swiperElem, function (key, elem) {
                             // console.log(elem);
@@ -194,73 +199,10 @@ function isVisible(ele) {
     return false;
 }
 
-
-function rotateplay() {
-            compatibility.requestAnimationFrame(rotateplay);
-            if (video.paused) video.play();
-
-            // Draw video overlay:
-            canvas.width = ~~(100 * video.videoWidth / video.videoHeight);
-            canvas.height = 100;
-            context.drawImage(video, 0, 0, canvas.clientWidth, canvas.clientHeight);
-
-            if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
-
-                // Prepare the detector once the video dimensions are known:
-                if (!detector) {
-                    var width = ~~(140 * video.videoWidth / video.videoHeight);
-                    var height = 140;
-                    detector = new objectdetect.detector(width, height, 1.1, objectdetect.handfist);
-                }
-
-                // Smooth rotation of the 3D object:
-                angle = smoother.smooth(angle);
-                document.getElementById('transform_a').setAttribute('rotation', '0 1 0 ' + angle[0]);
-                document.getElementById('transform_b').setAttribute('rotation', '1 0 0 ' + angle[1]);
-
-                // Perform the actual detection:
-                var coords = detector.detect(video, 1);
-
-                if (coords[0]) {
-                    var coord = coords[0];
-
-                    // Rescale coordinates from detector to video coordinate space:
-                    coord[0] *= video.videoWidth / detector.canvas.width;
-                    coord[1] *= video.videoHeight / detector.canvas.height;
-                    coord[2] *= video.videoWidth / detector.canvas.width;
-                    coord[3] *= video.videoHeight / detector.canvas.height;
-
-                    var fist_pos = [coord[0] + coord[2] / 2, coord[1] + coord[3] / 2];
-
-                    if (fist_pos_old) {
-                        var dx = (fist_pos[0] - fist_pos_old[0]) / video.videoWidth,
-                            dy = (fist_pos[1] - fist_pos_old[1]) / video.videoHeight;
-                            console.log(dx);
-                        if (dx*dx + dy*dy < 0.2) {
-                            angle[0] += 5.0 * dx;
-                            angle[1] += 5.0 * dy;
-                        }
-                        fist_pos_old = fist_pos;
-                    } else if (coord[4] > 2) {
-                        fist_pos_old = fist_pos;
-                    }
-
-                    // Draw coordinates on video overlay:
-                    context.beginPath();
-                    context.lineWidth = '2';
-                    context.fillStyle = fist_pos_old ? 'rgba(0, 255, 255, 0.5)' : 'rgba(255, 0, 0, 0.5)';
-                    context.fillRect(
-                        coord[0] / video.videoWidth * canvas.clientWidth,
-                        coord[1] / video.videoHeight * canvas.clientHeight,
-                        coord[2] / video.videoWidth * canvas.clientWidth,
-                        coord[3] / video.videoHeight * canvas.clientHeight);
-                    context.stroke();
-                } else fist_pos_old = null;
-            }
-        }
-
 function scroll(dx, dy) {
-    window.scrollBy(dx * 200, dy * 200);
+    if (!$("body").hasClass("disable-scroll")) {
+        window.scrollBy(dx * 200, dy * 200);
+    }
 }
 
 function showGesture(gesture) {
@@ -275,4 +217,3 @@ function showGesture(gesture) {
         $("#gestureDiv").fadeToggle("slow");
     }, 200);
 }
-
